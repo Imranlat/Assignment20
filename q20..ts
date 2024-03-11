48942ca767ae807f5ebe612afc309d94bd2160c7
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-03-11 20:42:20 +0500

    Add files via upload

---
+++ q20..ts
@@ -0,0 +1,8 @@
+//20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items
+
+// Creating a list of animals
+let animals: string[] = ["lion", "elephant", "tiger", "giraffe", "zebra", "cheetah", "monkey", "rhinoceros", "hippopotamus", "crocodile"];
+
+// Printing the list of animals
+console.log("List of animals:");
+animals.forEach(animal => console.log(animal));
